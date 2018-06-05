2dea0f63db9e20926763ed5d23ce6ef4dc48359c
Mitch Lembke <mitchell.lembke@gmail.com>
2018-06-05 11:19:51 -0600

    commented out env for mysql config

--- server.py
+++ server.py
@@ -20,11 +20,11 @@ app.config['SESSION_TYPE'] = 'filesystem'
 json = FlaskJSON(app)
 Session(app)
 
-MYSQL_HOST = os.environ.get('MYSQL_HOST')
-MYSQL_USER = os.environ.get('MYSQL_USER')
-MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
-MYSQL_DB = os.environ.get('MYSQL_DB')
-MYSQL_PORT = os.environ.get('MYSQL_PORT')
+# MYSQL_HOST = os.environ.get('MYSQL_HOST')
+# MYSQL_USER = os.environ.get('MYSQL_USER')
+# MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
+# MYSQL_DB = os.environ.get('MYSQL_DB')
+# MYSQL_PORT = os.environ.get('MYSQL_PORT')
 
 
 # MYSQL CONFIGURATION WHEN DEPLOYED
